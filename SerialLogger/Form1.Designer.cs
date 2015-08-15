81f9e39ff11a042977ab34e9767cc2235bbd0400
hiroyuki okada <ok.okada.hiroyuki@gmail.com>
2015-08-15 23:02:48 +0900

    first commit

---
+++ SerialLogger/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SerialLogger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.portlist = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // portlist
+            // 
+            this.portlist.FormattingEnabled = true;
+            this.portlist.Location = new System.Drawing.Point(107, 32);
+            this.portlist.Name = "portlist";
+            this.portlist.Size = new System.Drawing.Size(163, 20);
+            this.portlist.TabIndex = 0;
+            this.portlist.Click += new System.EventHandler(this.portlist_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "接続";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "シリアルポート";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(23, 63);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(549, 300);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(535, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 12);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Ver0.1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "アナログ",
+            "デジタル"});
+            this.comboBox1.Location = new System.Drawing.Point(389, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 375);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.portlist);
+            this.Name = "Form1";
+            this.Text = "SerialLogger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox portlist;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ SerialLogger/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SerialLogger
+{
+    public partial class Form1 : Form
+    {
+        Series series1 = new Series("data");
+        private int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            chart_init();
+            comboBox1.SelectedIndex = 0;
+        }
+
+
+        private void chart_init()
+        {
+            series1.ChartType = SeriesChartType.FastLine;
+            series1.Color = Color.Blue;
+            series1.BorderWidth = 2;
+            series1.IsVisibleInLegend = false;
+
+            chart1.Series.Add(series1);
+            chart1.ChartAreas[0].AxisX.Title = "X";
+
+            //X軸最小値、最大値、目盛間隔の設定
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 100;
+            chart1.ChartAreas[0].AxisX.Interval = 10;
+
+            chart1.ChartAreas[0].AxisY.Title = "Y";
+            chart1.ChartAreas[0].AxisY.Minimum = -100;
+            chart1.ChartAreas[0].AxisY.Maximum = 1100;
+            chart1.ChartAreas[0].AxisY.Interval = 100;
+        }
+
+        private void plot(int data)
+        {
+            series1.Points.Add(data);
+            if(count > 100)
+            {
+                series1.Points.RemoveAt(0);
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                button1.Text = "接続";
+            }
+            else
+            {
+                serialPort1.PortName = portlist.SelectedItem.ToString();
+                serialPort1.BaudRate = 115200;
+                try
+                {
+                    serialPort1.Open();
+                    button1.Text = "接続中";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = serialPort1.ReadLine();
+                if (!string.IsNullOrEmpty(data))
+                {
+                    //Invoke((MethodInvoker)(() => MessageBox.Show(data)));
+                    BeginInvoke(new AppendTextDelegate(packet_restortion), data);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        //delegate（処理を委譲）
+        delegate void AppendTextDelegate(string text);
+
+        //委譲先の関数
+        private void packet_restortion(string data)
+        {
+            int buf;
+            if (data.Length > 0)
+            {
+                // 改行コードの削除
+                data = data.Replace("\r", "").Replace("\n", "");
+                // カンマ区切りで分割して配列に格納する
+
+                string[] stArrayData = data.Split(',');
+                if (int.TryParse(stArrayData[0], out buf))
+                {
+                    plot(buf);
+                }
+            }
+        }
+
+        private void portlist_Click(object sender, EventArgs e)
+        {
+            string[] PortList = SerialPort.GetPortNames();
+
+            portlist.Items.Clear();
+
+            //! シリアルポート名をコンボボックスにセットする.
+            foreach (string PortName in PortList)
+            {
+                portlist.Items.Add(PortName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                series1.ChartType = SeriesChartType.StepLine;
+                chart1.ChartAreas[0].AxisY.Minimum = -1;
+                chart1.ChartAreas[0].AxisY.Maximum = 2;
+                chart1.ChartAreas[0].AxisY.Interval = 1;
+            }
+
+        }
+    }
+}
